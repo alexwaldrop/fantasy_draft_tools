37869ba813930abf739ec3e219745697b901ce32
alexwaldrop <alex.waldrop.jr@gmail.com>
2019-08-10 19:50:02 -0400

    Created python init_draftboard to work with downloaded ffanalytics datasets

--- python/constants.py
+++ python/constants.py
@@ -1,14 +1,28 @@
-NAME_FIELD = "Name"
-POS_FIELD = "pos"
+# Draftboard fields
+NAME_FIELD = "player"
+POS_FIELD = "position"
 POINTS_FIELD = "points"
-POINTS_SD_FIELD = "sd_pts"
+POINTS_SD_FIELD = "sdPts"
 ADP_FIELD = "adp"
 ADP_SD_FIELD = "sd_adp"
 ADP_TIMES_DRAFTED_FIELD = "adp_times_drafted"
 DRAFT_RANK_FIELD = "Draft Rank"
 DRAFT_SLOT_FIELD = "Draft Slot"
-VORP_FIELD = "points_vor"
+VORP_FIELD = "vor"
 VORP_RANK_FIELD = "VORP Rank"
 DRAFT_STATUS = "Drafted"
 MY_PICKS = "My Picks"
 RUN_SIM_DRAFT = "Potential Pick"
+
+# Team dict fields
+TEAM_STRING_FIELD = "players"
+STARTER_STRING_FIELD = "starters"
+TEAM_POINTS_FIELD = "player points"
+TEAM_VORP_FIELD = "player vorp"
+
+# MCMC team dict simulation fields
+SIM_STARTERS_PTS = "sim_starters_pts_avg"
+SIM_STARTERS_PTS_SD = "sim_starters_pts_sd"
+SIM_TEAM_VORP = "sim_team_vorp_avg"
+SIM_TEAM_VORP_SD = "sim_team_vorp_sd"
+SIM_COMPOSITE_SCORE = "total_value"
